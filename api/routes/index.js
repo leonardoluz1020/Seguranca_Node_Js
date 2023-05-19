fa00bf436c3a7926ae30287703f5b49d07ca3f49
Leonardo Luz <leonardoluz10@hotmail.com>
2023-05-19 19:17:04 -0300

    feat: :sparkles: add: feat, novo modelo, migration e router de usuarios

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser')
  
 const produto = require('./produtoRoute')
+const usuarios = require('./usuariosRoute')
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
-    produto
+    produto,
+    usuarios
   )
 }
